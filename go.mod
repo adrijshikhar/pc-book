7d5133a59721d753612407381f3f529a6d657049
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-12-18 21:16:38 +0530

    :zap: [go] Adds sample data for CPU

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang/protobuf v1.4.2
-	github.com/google/uuid v1.1.1 // indirect
+	github.com/google/uuid v1.1.1
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.0-beta.3 // indirect
 	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a // indirect
 	github.com/stretchr/testify v1.6.0 // indirect
--- sample/random.go
+++ sample/random.go
@@ -5,10 +5,39 @@ import (
 	"time"
 
 	"github.com/adrijshikhar/pc-book/pb"
+	"github.com/google/uuid"
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+}
+
+func randomStringFromSet(a ...string) string {
+	n := len(a)
+	if n == 0 {
+		return ""
+	}
+	return a[rand.Intn(n)]
+}
+
+func randomBool() bool {
+	return rand.Intn(2) == 1
+}
+
+func randomInt(min, max int) int {
+	return min + rand.Int()%(max-min+1)
+}
+
+func randomFloat64(min, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+func randomFloat32(min, max float32) float32 {
+	return min + rand.Float32()*(max-min)
+}
+
+func randomID() string {
+	return uuid.New().String()
 }
 
 func randomKeyboardLayout() pb.Keyboard_Layout {
@@ -22,6 +51,24 @@ func randomKeyboardLayout() pb.Keyboard_Layout {
 	}
 }
 
-func randomBool() bool {
-	return rand.Intn(2) == 1
+func randomCPUBrand() string {
+	return randomStringFromSet("Intel", "AMD")
+}
+
+func randomCPUName(brand string) string {
+	if brand == "Intel" {
+		return randomStringFromSet(
+			"Xeon E-2286M",
+			"Core i9-9980HK",
+			"Core i7-9750H",
+			"Core i5-9400F",
+			"Core i3-1005G1",
+		)
+	}
+
+	return randomStringFromSet(
+		"Ryzen 7 PRO 2700U",
+		"Ryzen 5 PRO 3500U",
+		"Ryzen 3 PRO 3200GE",
+	)
 }
